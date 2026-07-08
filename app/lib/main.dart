e87d93aef062262410e107b31cb36fab64cf4b8f
ocb <ocb@n1.home>
2026-07-08 17:02:47 +0200

    URL serveur définitive : antispam-1fa8eb98.runship.fr

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -77,7 +77,7 @@ class SetupScreen extends StatefulWidget {
 
 class _SetupScreenState extends State<SetupScreen> {
   final _url =
-      TextEditingController(text: 'https://antispam-85e4a1d2.runship.fr');
+      TextEditingController(text: 'https://antispam-1fa8eb98.runship.fr');
   final _key = TextEditingController();
   String? _error;
   bool _busy = false;
